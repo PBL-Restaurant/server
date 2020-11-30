dfc2ea7b3dbdf7fa3cdff99ac2234fb22fff6dff
Volcov Oleg <bojikch15@gmail.com>
2020-11-30 19:19:50 +0200

    Issue-2 Small fixes.
    + Rebased room check method in GET request.

--- routes/room.js
+++ routes/room.js
@@ -3,7 +3,7 @@ const Restaurant = require('../model/Restaurant');
 const TableRoom = require('../model/TableRoom');
 const verifyToken = require('./verifyToken');
 
-router.post('/check', verifyToken, async (req, res) => {
+router.get('/check', verifyToken, async (req, res) => {
     let room = await TableRoom.findOne({
         _id: req.body.tableRoomId
     });
